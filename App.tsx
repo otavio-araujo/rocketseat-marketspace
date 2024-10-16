d2bc29ddf6399147f138f1efe44f38b8717bfb1b
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-15 21:37:18 -0300

    Font Karla installed.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar } from "react-native"
+
+import {
+  useFonts,
+  Karla_400Regular,
+  Karla_700Bold,
+} from "@expo-google-fonts/karla"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Karla_400Regular,
+    Karla_700Bold,
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#EDECEE",
+      }}
+    >
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="#EDECEE"
+        translucent
+      />
+      {fontsLoaded ? <Text>Home</Text> : <View />}
+    </View>
+  )
+}
