cd6218fcaf0121a973503f4259d9c04e7f3ace39
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-22 23:47:11 -0300

    Calling signOut

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,12 +3,17 @@ import { createContext, ReactNode, useEffect, useState } from "react"
 import { api } from "@services/api"
 import { UserDTO } from "@dtos/UserDTO"
 
-import { storageUserSave, storageUserGet } from "@storage/storageUser"
+import {
+  storageUserSave,
+  storageUserGet,
+  storageUserRemove,
+} from "@storage/storageUser"
 
 export type AuthContextDataProps = {
   user: UserDTO
   isLoadingUserStorageData: boolean
   signIn: (email: string, password: string) => Promise<void>
+  signOut: () => Promise<void>
 }
 
 type AuthContextProviderProps = {
@@ -36,6 +41,18 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     }
   }
 
+  async function signOut() {
+    try {
+      setIsLoadingUserStorageData(true)
+      setUser({} as UserDTO)
+      await storageUserRemove()
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
   async function loadUserData() {
     try {
       const loggedUser = await storageUserGet()
@@ -53,7 +70,9 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     loadUserData()
   }, [])
   return (
-    <AuthContext.Provider value={{ user, signIn, isLoadingUserStorageData }}>
+    <AuthContext.Provider
+      value={{ user, signIn, isLoadingUserStorageData, signOut }}
+    >
       {children}
     </AuthContext.Provider>
   )
--- src/screens/app/LogOut.tsx
+++ src/screens/app/LogOut.tsx
@@ -2,12 +2,13 @@ import React, { useCallback } from "react"
 import { useFocusEffect, useNavigation } from "@react-navigation/native"
 
 import { AppNavigatorRoutesProps } from "@routes/app.routes"
+import { useAuth } from "@hooks/useAuth"
 
 export function LogOUt() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
-  function handleLogOut() {
-    console.log("LogOut")
-    navigation.navigate("home")
+  const { signOut } = useAuth()
+  async function handleLogOut() {
+    await signOut()
   }
 
   useFocusEffect(
